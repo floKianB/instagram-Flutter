7b533082e5a1da31d411699b278a0bcd65365214
floKianB <kianbaban@yahoo.com>
2022-09-01 19:07:16 -0400

    saved login page done

--- lib/savedLoginPage.dart
+++ lib/savedLoginPage.dart
@@ -16,9 +16,85 @@ class _savedLoginPageState extends State<savedLoginPage> {
         child: Column(
           children: [
             Expanded(
-              flex: 11,
+              flex: 14,
               child: Container(
-                child: Image.asset('images/switch_account_background.png')
+                child: Stack(
+                  children: [
+                    Image.asset('images/switch_account_background.png'),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        height: 350,
+                        width: 352,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: <Color> [
+                              Color.fromARGB(134, 255, 255, 255),
+                              Color.fromARGB(41, 255, 255, 255),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(15)
+                        ),
+                        
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: 130,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(60),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset('images/kian.jpeg')  
+                              )
+                            ),
+                            Text(
+                              'Kian Baban',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 1,
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xffF35383),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                minimumSize: Size(125, 46),
+                              ),
+                              onPressed: (){},
+                              child: Text(
+                                'Confirm',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 1,
+                            ),
+                            Text(
+                              'Switch Account',
+                              style: TextStyle(
+                                color: Colors.white,
+
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
